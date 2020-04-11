799f416ff19f1c12d0aadb1eee33a258d332a112
Marc Cataford <c.marcandre@gmail.com>
2020-04-11 12:47:53 -0400

    refactor: more natural ordering of params

--- src/commands.rs
+++ src/commands.rs
@@ -9,13 +9,13 @@ pub fn exec(state: memory::State, command: &str) -> memory::State {
 
 fn exec_fragment(state: memory::State, fragment: regex::Captures) -> memory::State {
     match &fragment["op"] {
-        "add" => add(state, &fragment["rs"], &fragment["rt"], &fragment["rd"]),
+        "add" => add(state, &fragment["rd"], &fragment["rs"], &fragment["rt"]),
         "li" => load_immediate(state, &fragment["rd"], &fragment["imm"]),
         _ => catchall(state),
     }
 }
 
-fn add(state: memory::State, rs: &str, rt: &str, rd: &str) -> memory::State {
+fn add(state: memory::State, rd: &str, rs: &str, rt: &str) -> memory::State {
     let rs_value = state.get_register(rs);
     let rt_value = state.get_register(rt);
     let resulting_value = rs_value + rt_value;

f49ed7ad3255c5e8ce1e248dceee12b2462096da
Marc Cataford <c.marcandre@gmail.com>
2020-04-11 12:55:53 -0400

    wip: addi support

--- src/commands.rs
+++ src/commands.rs
@@ -10,9 +10,15 @@ pub fn exec(state: memory::State, command: &str) -> memory::State {
 fn exec_fragment(state: memory::State, fragment: regex::Captures) -> memory::State {
     match &fragment["op"] {
         "add" => add(state, &fragment["rd"], &fragment["rs"], &fragment["rt"]),
+        "addi" => add_immediate(state, &fragment["rd"], &fragment["rs"], &fragment["imm"]),
         "li" => load_immediate(state, &fragment["rd"], &fragment["imm"]),
+
         _ => catchall(state),
     }
+}
+
+fn parse_immediate(imm: &str) -> i32 {
+    return imm.parse().unwrap();
 }
 
 fn add(state: memory::State, rd: &str, rs: &str, rt: &str) -> memory::State {
@@ -22,8 +28,14 @@ fn add(state: memory::State, rd: &str, rs: &str, rt: &str) -> memory::State {
     return memory::write_to_register(state, rd, resulting_value);
 }
 
+fn add_immediate(state: memory::State, rd: &str, rs: &str, imm: &str) -> memory::State {
+    let rs_value = state.get_register(rs);
+    let resulting_value = rs_value + parse_immediate(imm);
+    return memory::write_to_register(state, rd, resulting_value);
+}
+
 fn load_immediate(state: memory::State, rd: &str, imm: &str) -> memory::State {
-    return memory::write_to_register(state, rd, imm.parse().unwrap());
+    return memory::write_to_register(state, rd, parse_immediate(imm));
 }
 
 fn catchall(state: memory::State) -> memory::State {
